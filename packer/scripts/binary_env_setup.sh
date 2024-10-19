e77406858357ff24d1c0644144817ffa3a03b440
Gaurav Gunjal <gunjal.ga@northeastern.edu>
2024-10-19 01:22:11 -0400

    packer changes to move db on RDS

--- packer/scripts/binary_env_setup.sh
+++ packer/scripts/binary_env_setup.sh
@@ -5,4 +5,3 @@ sudo mv /tmp/webapp /usr/bin/
 # Change the ownership of the webapp file to csye6225
 sudo chown csye6225:csye6225 /usr/bin/webapp
 sudo chmod +x /usr/bin/webapp
-sudo mv /tmp/.env /usr/bin/
