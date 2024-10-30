0c7d6d51996bb98c87f7ecb56178f0686ea4476f
Gaurav Gunjal <gunjal.ga@northeastern.edu>
2024-10-30 16:45:58 -0400

    added json logging

--- repository/profile-pic-repository.go
+++ repository/profile-pic-repository.go
@@ -11,13 +11,40 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/csye-6225-gaurav/webapp/models"
 	"github.com/csye-6225-gaurav/webapp/storage"
+	"github.com/csye-6225-gaurav/webapp/utils"
 	"github.com/gofiber/fiber/v2"
+	zlog "github.com/rs/zerolog/log"
 )
 
 func SaveProfilePic(ctx *fiber.Ctx) error {
 
 	user := ctx.Locals("user").(models.User)
-	//TODO:check if profile pic already exists
+	endpoint := ctx.Path()
+	form, err := ctx.MultipartForm()
+	if err != nil {
+		zlog.Error().Err(err).Str("endpoint", endpoint).Msg("Failed to parse form data")
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Failed to parse form data",
+		})
+	}
+	for key := range form.File {
+		if key != "profilePic" {
+			ctx.Status(fiber.StatusBadRequest)
+			return nil
+		}
+	}
+	if len(form.Value) > 0 {
+		ctx.Status(fiber.StatusBadRequest)
+		return nil
+	}
+	var image models.Image
+	err = storage.DB.Where("user_id = ?", user.ID).First(&image).Error
+	if err == nil {
+		zlog.Warn().Str("endpoint", endpoint).Str("user_id", user.ID.String()).Msg("Profile picture already exists")
+		return ctx.Status(fiber.StatusConflict).JSON(fiber.Map{
+			"message": "Profile picture already exists",
+		})
+	}
 	file, err := ctx.FormFile("profilePic")
 	if err != nil {
 		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
@@ -43,25 +70,28 @@ func SaveProfilePic(ctx *fiber.Ctx) error {
 	fileName := fmt.Sprintf("%s/%s", user.ID.String(), file.Filename)
 
 	// Upload to S3
+	start := time.Now()
 	_, err = storage.S3Client.PutObject(ctx.Context(), &s3.PutObjectInput{
 		Bucket: &bucketName, // replace with your bucket name
 		Key:    &fileName,
 		Body:   bytes.NewReader(buf.Bytes()),
 	})
-	log.Println("file uploaded successfully")
+	utils.Client.PrecisionTiming("s3.put", time.Since(start))
 	if err != nil {
-		log.Println(err)
+		zlog.Error().Err(err).Str("endpoint", endpoint).Msg("Failed to upload file to S3")
 		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 			"message": "Failed to upload file to S3",
 		})
 	}
-	image := models.Image{
+	zlog.Info().Str("endpoint", endpoint).Str("user_id", user.ID.String()).Msg("File uploaded successfully")
+	image = models.Image{
 		FileName:   file.Filename,
 		URL:        fmt.Sprintf("https://%s/%s", os.Getenv("S3_Bucket"), fileName),
 		UploadDate: time.Now(),
 		UserID:     user.ID,
 	}
 	if err := storage.DB.Create(&image).Error; err != nil {
+		zlog.Error().Err(err).Str("endpoint", endpoint).Msg("Failed to save image metadata")
 		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 			"message": "Failed to save image metadata",
 		})
@@ -72,28 +102,33 @@ func SaveProfilePic(ctx *fiber.Ctx) error {
 
 func GetProfilePic(ctx *fiber.Ctx) error {
 	user := ctx.Locals("user").(models.User)
+	endpoint := ctx.Path()
 	var image models.Image
 	err := storage.DB.Where("user_id = ?", user.ID).First(&image).Error
 	if err != nil {
+		zlog.Warn().Err(err).Str("endpoint", endpoint).Str("user_id", user.ID.String()).Msg("Image not found")
 		if strings.Contains(err.Error(), "record not found") {
-			ctx.Status(fiber.StatusInternalServerError).JSON(&fiber.Map{"message": "Image not found"})
+			ctx.Status(fiber.StatusNotFound).JSON(&fiber.Map{"message": "Image not found"})
 		}
 		log.Println("Image not found:", err)
 		return nil
 	}
-
+	zlog.Info().Str("endpoint", endpoint).Str("user_id", user.ID.String()).Msg("Profile picture fetched successfully")
 	ctx.Status(fiber.StatusOK).JSON(image)
 	return nil
 }
 
 func DeleteProfilePic(ctx *fiber.Ctx) error {
 	user := ctx.Locals("user").(models.User)
+	endpoint := ctx.Path()
 	var image models.Image
 
 	err := storage.DB.Where("user_id = ?", user.ID).First(&image).Error
 	if err != nil {
+		zlog.Warn().Err(err).Str("endpoint", endpoint).Str("user_id", user.ID.String()).Msg("Failed to fetch image metadata")
 		if strings.Contains(err.Error(), "record not found") {
-			return ctx.Status(fiber.StatusNotFound).JSON(fiber.Map{"message": "Image not found"})
+			ctx.Status(fiber.StatusNotFound)
+			return nil
 		}
 		log.Println("Failed to fetch image metadata:", err)
 		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message": "Failed to fetch image metadata"})
@@ -101,19 +136,22 @@ func DeleteProfilePic(ctx *fiber.Ctx) error {
 
 	bucketName := os.Getenv("Bucket_Name")
 	filePath := fmt.Sprintf("%s/%s", user.ID.String(), image.FileName)
+	start := time.Now()
 	_, err = storage.S3Client.DeleteObject(ctx.Context(), &s3.DeleteObjectInput{
 		Bucket: &bucketName,
 		Key:    &filePath,
 	})
+	utils.Client.PrecisionTiming("s3.Delete", time.Since(start))
 	if err != nil {
-		log.Println("Failed to delete image from S3:", err)
+		zlog.Error().Err(err).Str("endpoint", endpoint).Str("user_id", user.ID.String()).Msg("Failed to delete image from S3")
 		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message": "Failed to delete image from S3"})
 	}
-
+	zlog.Info().Str("endpoint", endpoint).Str("user_id", user.ID.String()).Msg("Image deleted from S3")
 	if err := storage.DB.Delete(&image).Error; err != nil {
-		log.Println("Failed to delete image metadata:", err)
+		zlog.Error().Err(err).Str("endpoint", endpoint).Str("user_id", user.ID.String()).Msg("Failed to delete image metadata")
 		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message": "Failed to delete image metadata"})
 	}
 
-	return ctx.Status(fiber.StatusOK).JSON(fiber.Map{"message": "Profile picture deleted successfully"})
+	ctx.Status(fiber.StatusNoContent)
+	return nil
 }
